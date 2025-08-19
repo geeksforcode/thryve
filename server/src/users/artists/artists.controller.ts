edefc1aae55a984f7f84e90a4574c288c45b921f
collocoder <itscollinsmaina@gmail.com>
2025-08-19 17:12:53 +0300

    Add artist DTO and update artists module

--- server/src/users/artists/artists.controller.ts
+++ server/src/users/artists/artists.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { ArtistsService } from './artists.service';
+import { CreateArtistDto, UpdateArtistDto } from './dto/artist.dto';
 
 @Controller('artists')
-export class ArtistsController {}
+export class ArtistsController {
+  constructor(private readonly artistsService: ArtistsService) {}
+
+  @Get()
+  findAll() {
+    return this.artistsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.artistsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistsService.create(createArtistDto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+    return this.artistsService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.artistsService.remove(id);
+  }
+}
--- server/src/users/artists/artists.service.ts
+++ server/src/users/artists/artists.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateArtistDto, UpdateArtistDto } from './dto/artist.dto';
+
+// Temporary in-memory storage (replace with DB integration later)
+const artists = new Map<string, any>();
 
 @Injectable()
-export class ArtistsService {}
+export class ArtistsService {
+  findAll() {
+    return Array.from(artists.values());
+  }
+
+  findOne(id: string) {
+    const artist = artists.get(id);
+    if (!artist) throw new NotFoundException(`Artist with id ${id} not found`);
+    return artist;
+  }
+
+  create(createArtistDto: CreateArtistDto) {
+    const id = (Math.random() * 100000).toFixed(0);
+    const newArtist = { id, ...createArtistDto };
+    artists.set(id, newArtist);
+    return newArtist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto) {
+    if (!artists.has(id)) {
+      throw new NotFoundException(`Artist with id ${id} not found`);
+    }
+    const updatedArtist = { ...artists.get(id), ...updateArtistDto };
+    artists.set(id, updatedArtist);
+    return updatedArtist;
+  }
+
+  remove(id: string) {
+    if (!artists.has(id)) {
+      throw new NotFoundException(`Artist with id ${id} not found`);
+    }
+    const deleted = artists.get(id);
+    artists.delete(id);
+    return deleted;
+  }
+}
